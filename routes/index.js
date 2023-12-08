5bd802783a1dddd5e8c0c65ea21dfbdf30e38f84
Catelyn Richard <catelyn.richard@yahoo.com>
2023-12-08 15:09:59 -0500

    users controllers correction

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const users = require("./users");
 const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/items", clothingItems);
-//router.use("/users", users);
+router.use("/users", users);
 
 router.use((req, res) => {
   res.status(NOT_FOUND).send({
